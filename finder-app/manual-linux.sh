79b3bf83635a38eef091a871309c6bc42896ed21
Tyler <ttmowry@gmail.com>
2025-02-05 22:05:06 -0500

    last commit jesus christ this compiler toolchain doesn't work with the kernel build fix plzzzzzzz

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -28,9 +28,11 @@ if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+
+    
 fi
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-    cd linux-stable
+    cd ${OUTDIR}/linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
